e6dcabc4da736a4dabab24afd645c5079ee318d3
Shubhanshu Kakkar <kakkarhimanshu889@gmail.com>
2024-03-01 01:34:27 +0530

    basic project structure created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage';
+const router = createBrowserRouter([{
+path:"/",
+element:<Layout/>,
+errorElement:<ErrorPage/>
+
+}])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello world!</h1>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
