9129dd3e5d56253041e2e9cfcba4b49b51e41f26
Shubhanshu Kakkar <kakkarhimanshu889@gmail.com>
2024-03-02 03:03:47 +0530

    routing and navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/logo.jpeg";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav>
+      <div className="container nav__container">
+        <Link to="/" className="nav__logo">
+          <img src={Logo} alt="NavBar Logo" />
+        </Link>
+        <ul className="nav__menu">
+          <li>
+            <Link to="/profile"> Profile</Link>
+            <Link to="/create"> create</Link>
+            <Link to="/authors">Authors</Link>
+            <Link to="/logout">Logout</Link>
+            <button className="nav__toggle-btn"></button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,39 @@ import './index.css';
 import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
 import ErrorPage from './pages/ErrorPage';
+import PostDetail from './pages/PostDetail';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import UserProfile from './pages/UserProfile';
+import EditPost from './pages/EditPost';
+import CategoryPosts from './pages/CategoryPosts';
+import AuthorPosts from './pages/AuthorPosts';
+import Dashboard from './pages/Dashboard';
+import Logout from './pages/Logout';
+import CreatePost from './pages/CreatePost';
+import Authors from './pages/Authors';
+
+
 const router = createBrowserRouter([{
 path:"/",
 element:<Layout/>,
 errorElement:<ErrorPage/>,
-// children=:[]
+ children:[
+  {index: true , element : <Home/>},
+  {path:"posts/id", element : <PostDetail/>},
+  {path:"register", element :<Register/>},
+  {path:"login", element :<Login/>},
+  {path:"profile/:id", element :<UserProfile/>},
+  {path:"authors", element :<Authors/>},
+  {path:"create", element :<CreatePost/>},
+  {path:"posts/:id/edit", element :<EditPost/>},
+  {path:"posts/users/:id", element :<AuthorPosts/>},
+  {path:"posts/categories/:categories", element :<CategoryPosts/>},
+  {path:"myposts/:id", element :<Dashboard/>},
+  {path:"logout", element :<Logout/>}
+
+ ]
 
 }])
 
